a576b41d5940ed1b7ef2777d5c01c12473a49fb1
Manuel Thalmann <m@nuth.ch>
2023-08-02 16:16:46 +0200

    Remove obsolete type declarations

--- packages/babel-macro/ts-nameof.macro.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "@typescript-nameof/babel-macro" {
-    import { INameOfProvider } from "@typescript-nameof/core";
-
-    /**
-     * A component for parsing the names of identifiers and expressions.
-     */
-    const nameof: INameOfProvider;
-    export default nameof;
-}
