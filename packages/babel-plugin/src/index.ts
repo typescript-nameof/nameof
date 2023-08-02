87e227bd1395d59f968a104174a23880f4c02305
Manuel Thalmann <m@nuth.ch>
2023-08-02 16:20:56 +0200

    Rename the `nameof` babel plugin

--- packages/babel-plugin/src/index.ts
+++ packages/babel-plugin/src/index.ts
@@ -1,4 +1,4 @@
-import { plugin } from "@typescript-nameof/babel-transformer";
+import { plugin as nameofPlugin } from "@typescript-nameof/babel-transformer";
 
 // eslint-disable-next-line import/no-default-export
-export default plugin;
+export default nameofPlugin;
