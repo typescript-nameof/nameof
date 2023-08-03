a8b061da6b8ec7fa37338fca1c4ef72b7060ce91
Manuel Thalmann <m@nuth.ch>
2023-08-03 10:27:12 +0200

    Remove unnecessary files

--- packages/test/src/common/index.ts
+++
@@ -1,2 +0,0 @@
-nameof<Console>((c) => c.log);
-nameof<Console>((c) => c);
--- packages/test/src/macro/index.ts
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import nameof from "@typescript-nameof/babel-macro";
-
-nameof<Console>((c) => c.log);
-nameof<Console>((c) => c);
