793c8cc867c4af20138445728192e03ad4e15539
Manuel Thalmann <m@nuth.ch>
2023-08-10 22:49:34 +0200

    Add a class for providing babel features

---
+++ packages/babel-transformer/src/Transformation/BabelFeatures.ts
@@ -0,0 +1,41 @@
+import type babelAPI = require("@babel/core");
+import { TransformerFeatures } from "@typescript-nameof/common";
+
+/**
+ * @inheritdoc
+ */
+export abstract class BabelFeatures extends TransformerFeatures<babelAPI.NodePath>
+{
+    /**
+     * An instance of the babel compiler.
+     */
+    private babel: typeof babelAPI;
+
+    /**
+     * Initializes a new instance of the {@linkcode BabelFeatures} class.
+     *
+     * @param babel
+     * An instance of the babel compiler.
+     */
+    public constructor(babel: typeof babelAPI)
+    {
+        super();
+        this.babel = babel;
+    }
+
+    /**
+     * Gets an instance of the babel compiler.
+     */
+    public get Babel(): typeof babelAPI
+    {
+        return this.babel;
+    }
+
+    /**
+     * Gets a component for handling babel types.
+     */
+    public get Types(): typeof babelAPI.types
+    {
+        return this.Babel.types;
+    }
+}
