3cfff2ad8ff84393bd979ce142bd671aa19eb1af
Manuel Thalmann <m@nuth.ch>
2023-08-19 06:31:33 +0200

    Implement `babel-macro` using a transformer class

---
+++ packages/babel-macro/src/BabelMacroTransformer.ts
@@ -0,0 +1,75 @@
+import babel = require("@babel/core");
+import { BabelTransformer } from "@typescript-nameof/babel-plugin";
+import { BabelContext } from "@typescript-nameof/babel-plugin/src/Transformation/BabelContext";
+import { IErrorHandler } from "@typescript-nameof/common";
+import { MacroError } from "babel-plugin-macros";
+
+/**
+ * Provides the functionality to transform files using a babel-macro.
+ */
+export class BabelMacroTransformer extends BabelTransformer
+{
+    /**
+     * Initializes a new instance of the {@linkcode BabelMacroTransformer} class.
+     *
+     * @param babelAPI
+     * An instance of babel.
+     *
+     * @param errorHandler
+     * A component for handling errors.
+     */
+    public constructor(babelAPI: typeof babel, errorHandler?: IErrorHandler<babel.Node, BabelContext>)
+    {
+        super(babel, errorHandler);
+    }
+
+    /**
+     * Gets a babel-macro for transforming `nameof` calls.
+     */
+    public get Macro(): any
+    {
+        return (macroContext: any): void =>
+        {
+            this.MonitorInterpolations(
+                (context) =>
+                {
+                    (macroContext.references.default as Array<babel.NodePath<babel.types.Identifier>>).slice().reverse().forEach(
+                        (path) =>
+                        {
+                            let babelContext: BabelContext = {
+                                ...context,
+                                state: macroContext.state,
+                                nameofIdentifierName: path.node.name
+                            };
+
+                            this.TransformNode(getPath(), babelContext);
+
+                            /**
+                             * Gets the path to the node to transform.
+                             *
+                             * @returns
+                             * The path to the node to transform.
+                             */
+                            function getPath(): any
+                            {
+                                const parentPath = path.parentPath; // identifier;
+
+                                if (parentPath.type === "CallExpression")
+                                {
+                                    return parentPath;
+                                }
+
+                                const grandParentPath = parentPath.parentPath;
+
+                                if (parentPath.type === "MemberExpression" && grandParentPath?.type === "CallExpression")
+                                {
+                                    return grandParentPath;
+                                }
+
+                                throw new MacroError(`[ts-nameof]: Could not find a call expression at path: ${grandParentPath?.getSource()}`);
+                            }
+                        });
+                });
+        };
+    }
+}
--- packages/babel-macro/src/index.ts
+++ packages/babel-macro/src/index.ts
@@ -1,9 +1,7 @@
 /// <reference path="references.d.ts" />
-import { BabelTransformer } from "@typescript-nameof/babel-plugin";
-import { BabelContext } from "@typescript-nameof/babel-plugin/src/Transformation/BabelContext";
-import { UnusedInterpolationError } from "@typescript-nameof/common";
 import { INameOfProvider } from "@typescript-nameof/common-types";
-import { createMacro, MacroError } from "babel-plugin-macros";
+import { createMacro } from "babel-plugin-macros";
+import { BabelMacroTransformer } from "./BabelMacroTransformer";
 
 const nameof: INameOfProvider = createMacro(nameofMacro);
 // eslint-disable-next-line import/no-default-export
@@ -18,56 +16,5 @@ export default nameof;
 // @ts-ignore
 function nameofMacro(context: any): void
 {
-    let transformer = new BabelTransformer(context.babel);
-
-    let transformerContext: BabelContext = {
-        state: context.state,
-        interpolationCalls: []
-    };
-
-    // go over in reverse as if traversing in post order
-    const reverseDefault = context.references.default.slice().reverse();
-
-    // @ts-ignore
-    reverseDefault.forEach(path =>
-    {
-        transformer.TransformNode(
-            getPath(),
-            {
-                ...transformerContext,
-                nameofIdentifierName: path.node.name
-            });
-
-        /**
-         * Gets the path to the node to transform.
-         *
-         * @returns
-         * The path to the node to transform.
-         */
-        function getPath(): any
-        {
-            const parentPath = path.parentPath; // identifier;
-
-            if (parentPath.type === "CallExpression")
-            {
-                return parentPath;
-            }
-
-            const grandParentPath = parentPath.parentPath;
-
-            if (parentPath.type === "MemberExpression" && grandParentPath.type === "CallExpression")
-            {
-                return grandParentPath;
-            }
-
-            throw new MacroError(`[ts-nameof]: Could not find a call expression at path: ${grandParentPath.getSource()}`);
-        }
-    });
-
-    let remainingCall = context.interpolationCalls?.[0];
-
-    if (remainingCall)
-    {
-        throw new UnusedInterpolationError(transformer.Transformer, remainingCall, context);
-    }
+    return new BabelMacroTransformer(context.babel).Macro(context);
 }
