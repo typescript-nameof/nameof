3ed252299ee9401cdb5b06124908d780cae7efa4
Manuel Thalmann <m@nuth.ch>
2023-08-19 06:42:23 +0200

    Use proper implementation for each ecosystem

--- packages/nameof/src/main.ts
+++ packages/nameof/src/main.ts
@@ -4,26 +4,42 @@ import { ProgramPattern, TransformerExtras } from "ts-patch";
 import { Program, SourceFile, TransformationContext, TransformerFactory } from "typescript";
 import { Api } from "./Api";
 import { replaceInFiles, replaceInText } from "./text";
+import { TSJestTransformer } from "./Transformation/TSJestTransformer";
+import { TSPatchTransformer } from "./Transformation/TSPatchTransformer";
 import { TypeScriptTransformer } from "./Transformation/TypeScriptTransformer";
-
-let transformerFactory = new TypeScriptTransformer().Factory;
 
 let transformerFactoryBuilder: ProgramPattern & TransformerFactory<SourceFile> = (...args: [Program, Record<string, unknown>?, TransformerExtras?] | [TransformationContext]) =>
 {
-    if ("factory" in args[0])
+    let factory: TransformerFactory<SourceFile>;
+
+    if (args[2]?.addDiagnostic)
     {
-        return transformerFactory(args[0]) as any;
+        factory = new TSPatchTransformer(args[2]).Factory;
     }
     else
     {
-        return transformerFactory;
+        factory = new TypeScriptTransformer().Factory;
+    }
+
+    if ("factory" in args[0])
+    {
+        return factory(args[0]) as any;
+    }
+    else
+    {
+        return factory;
     }
 };
 
 const api: Api = transformerFactoryBuilder as Api;
 api.replaceInFiles = replaceInFiles;
 api.replaceInText = replaceInText;
+
 // this is for ts-jest support... not ideal
-api.factory = (compiler: TsCompilerInstance) => transformerFactory;
+api.factory =
+    (compiler: TsCompilerInstance) =>
+    {
+        return new TSJestTransformer(compiler).Factory;
+    };
 
 export = api;
