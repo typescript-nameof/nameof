22a0e0ef881329670a317fd288394a8b0db93869
Manuel Thalmann <m@nuth.ch>
2023-08-22 15:40:57 +0200

    Copy files during `mocha` tests

--- packages/nameof/src/tests/text/index.test.ts
+++ packages/nameof/src/tests/text/index.test.ts
@@ -1,6 +1,10 @@
-import { basename } from "path";
+import { basename, join } from "path";
+import { fileURLToPath } from "url";
+import fs from "fs-extra";
 import { IssueTests } from "./issuesTests.js";
 import { ReplaceInFilesTests } from "./replaceInFilesTests.js";
+
+const { copy, emptyDir } = fs;
 
 /**
  * Registers tests for text components.
@@ -11,6 +15,16 @@ export function TextTests(): void
         basename(new URL(".", import.meta.url).pathname),
         () =>
         {
+            suiteSetup(
+                async () =>
+                {
+                    let projectRoot = join(fileURLToPath(new URL(".", import.meta.url)), "..", "..", "..");
+                    let sourceRoot = join(projectRoot, "src", "tests", "testFiles");
+                    let targetRoot = join(projectRoot, "temp", "testFiles");
+                    await emptyDir(targetRoot);
+                    await copy(sourceRoot, targetRoot);
+                });
+
             IssueTests();
             ReplaceInFilesTests();
         });
