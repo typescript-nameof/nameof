f9fe28a279a635109c1e650737bd6c2453a1e109
Manuel Thalmann <m@nuth.ch>
2023-08-25 04:08:09 +0200

    Add further `IndexAccessNode` tests

--- packages/common/src/tests/Serialization/IndexAccessNode.test.ts
+++ packages/common/src/tests/Serialization/IndexAccessNode.test.ts
@@ -1,4 +1,4 @@
-import { strictEqual } from "assert";
+import { deepStrictEqual, strictEqual } from "assert";
 import { nameOf } from "ts-nameof-proxy";
 import { IdentifierNode } from "../../Serialization/IdentifierNode.cjs";
 import { IndexAccessNode } from "../../Serialization/IndexAccessNode.cjs";
@@ -18,11 +18,17 @@ export function IndexAccessNodeTests(): void
         () =>
         {
             let expression: ParsedNode<any>;
+            let root: IdentifierNode<any>;
+            let innerAccessor: IndexAccessNode<any>;
+            let outerAccessor: IndexAccessNode<any>;
 
             setup(
                 () =>
                 {
                     expression = new IdentifierNode({}, "window");
+                    root = new IdentifierNode({}, "console");
+                    innerAccessor = new IndexAccessNode({}, root, new StringLiteralNode({}, "log"));
+                    outerAccessor = new IndexAccessNode({}, innerAccessor, new StringLiteralNode({}, "bind"));
                 });
 
             suite(
@@ -67,6 +73,32 @@ export function IndexAccessNodeTests(): void
                             strictEqual(part.type, PathKind.Unsupported);
                             strictEqual(part.isAccessor, true);
                             strictEqual(part.source, expression.Source);
+                        });
+                });
+
+            suite(
+                nameOf<IndexAccessNode<any>>((node) => node.Path),
+                () =>
+                {
+                    test(
+                        "Checking whether the full path including the path of the underlying expression is returned…",
+                        () =>
+                        {
+                            deepStrictEqual(innerAccessor.Path, [root.PathPart, innerAccessor.PathPart]);
+                            deepStrictEqual(outerAccessor.Path, [root.PathPart, innerAccessor.PathPart, outerAccessor.PathPart]);
+                        });
+                });
+
+            suite(
+                nameOf<IndexAccessNode<any>>((node) => node.Root),
+                () =>
+                {
+                    test(
+                        "Checking whether the root of the access path is returned…",
+                        () =>
+                        {
+                            strictEqual(innerAccessor.Root, root);
+                            strictEqual(outerAccessor.Root, root);
                         });
                 });
         });
