da0c0d86f22adab606697190bd84fe778afd5e7a
Manuel Thalmann <m@nuth.ch>
2023-08-28 01:33:50 +0200

    Fix broken files

--- packages/types/type-tests/nameof.test-d.ts
+++ packages/types/type-tests/nameof.test-d.ts
@@ -1,4 +1,4 @@
-/// <reference path="../index.d.ts" />
+/// <reference path="../index.d.cts" />
 import { INameOfProvider } from "@typescript-nameof/common-types";
 import { expectType } from "tsd";
 
