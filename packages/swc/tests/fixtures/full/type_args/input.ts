5045f9f88cff2fc40b55c9134ed215a459c8aa76
Manuel Thalmann <m@nuth.ch>
2025-09-04 00:31:44 +0200

    Print computed properties properly

--- packages/swc/tests/fixtures/full/type_args/input.ts
+++ packages/swc/tests/fixtures/full/type_args/input.ts
@@ -2,6 +2,7 @@ nameof<NodeJS.ReadStream>();
 nameof.full<(NodeJS.Immediate)>();
 nameof.full<import("readline").Interface["close"]>();
 nameof.full<typeof process.abort>();
+nameof.full<typeof process.argv[0]>();
 
 class Test {
     toString() {
